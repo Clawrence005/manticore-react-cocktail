fa4dc729213593804c2cbc1ad24f222632b851ca
colin <clawrence005@yahoo.com>
2020-09-19 21:19:35 -0500

    adding mongoose connection config

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 
+const connectDb = require('./dbConfig')
+connectDb();
+
 const app = express();
+
+// app.use()
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on PORT ${PORT}`.blue.bold))
+
