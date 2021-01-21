f299aec88d8872a83f71dcdbc292afe62c299487
colin <clawrence005@yahoo.com>
2021-01-20 21:03:04 -0600

    added working user form using hooks, modify later to refresh

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [users, setUsers] = useState([]);
 
+  const [users, setUsers] = useState([]);
   useEffect(() => {
     axios
       .get("http://localhost:5000/api/users")
@@ -13,9 +13,7 @@ function App() {
       .catch((err) => console.log(err))
   }, []);
 
-  // const [beers, setBeers] = useState([]);
   const [cocktails, setCocktails] = useState([]);
-
   useEffect(() => {
     axios
       .get("http://localhost:5000/api/cocktails")
@@ -23,9 +21,91 @@ function App() {
       .catch((err) => console.log(err))
   }, [])
 
+  // const [state, setState] = useState({
+  //   userName: "",
+  //   email: "",
+  //   bio: "",
+  //   userImage: "",
+  // });
+
+  const [userName, setUserName] = useState('');
+  const [email, setEmail] = useState('');
+  const [bio, setBio] = useState('');
+  const [userImage, setUserImage] = useState('');
+
+  // const [inputValue, setInputValue] = useState({
+  //   userName: '', bio: '', email: '', userImage: ''
+  // });
+
+  // const [inputUserName, setInputUserName] = useState({});
+
+  // const handlerUserName = event => {
+  //   setInputUserName(event.target.value);
+  // };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // alert(`
+    // submitting: 
+    // userName: ${userName}
+    // email: ${email}
+    // bio: ${bio}
+    // userImage: ${userImage}
+    // `)
+
+    const newUser = {
+      userName,
+      email,
+      bio,
+      userImage,
+    }
+    axios.post('http://localhost:5000/api/users', newUser)
+      .then(res => console.log(res.data));
+
+  }
   return (
     <div className="App">
       <h1>Manticore</h1>
+      <form onSubmit={handleSubmit}>
+        <h2>User Form</h2>
+        <br />
+        <label>Name</label>
+        <input
+          type="text"
+          name="userName"
+          value={userName}
+          onChange={e => setUserName(e.target.value)}
+        />
+        <br />
+
+        <label>email</label>
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+
+        <label>bio</label>
+        <input
+          type="text"
+          name="bio"
+          value={bio}
+          onChange={e => setBio(e.target.value)} />
+        <br />
+
+        <label>image</label>
+        <input
+          type="text"
+          name="userImage"
+          value={userImage}
+          onChange={e => setUserImage(e.target.value)} />
+        <br />
+
+        <button type="submit">submit</button>
+      </form>
+
       <h2>users</h2>
       {users.map((user) => <div className="card" key={user._id}>
         <div ><strong>{user.userName}</strong></div>
